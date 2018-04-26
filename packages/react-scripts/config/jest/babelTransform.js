0b13209c63e8a691429d527ce3f52f568acce05e
Yu-Hsin Lu <kerol2r20@gmail.com>
2018-04-27 03:09:27 +0800

    Add features for cra
    * Replace standard from eslinit
    * Add support decorator and pipeline operator syntax

--- packages/react-scripts/config/jest/babelTransform.js
+++ packages/react-scripts/config/jest/babelTransform.js
@@ -1,18 +1,15 @@
-// @remove-on-eject-begin
+// @remove-file-on-eject
 /**
  * Copyright (c) 2014-present, Facebook, Inc.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-// @remove-on-eject-end
 'use strict';
 
 const babelJest = require('babel-jest');
 
 module.exports = babelJest.createTransformer({
-  presets: [require.resolve('babel-preset-react-app')],
-  // @remove-on-eject-begin
+  presets: [require.resolve('@babel/preset-env'), require.resolve('@babel/preset-react')],
   babelrc: false,
-  // @remove-on-eject-end
 });
